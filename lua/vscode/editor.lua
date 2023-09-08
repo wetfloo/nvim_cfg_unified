3fd77c69bc5dea689bd790af2f7d731665ed2df4
wetfloo <yahahaha.1234567890@gmail.com>
2023-09-09 03:23:26 +0700

    Close side bars in one action

--- lua/vscode/editor.lua
+++ lua/vscode/editor.lua
@@ -49,6 +49,14 @@ vscode_map({ 'n', 'x' }, '<C-l>', 'workbench.action.focusRightGroup', { desc = '
 vscode_map({ 'n', 'x' }, '<A-l>', 'workbench.action.nextEditor')
 vscode_map({ 'n', 'x' }, '<A-h>', 'workbench.action.previousEditor')
 vscode_map({ 'n', 'x' }, '<A-d>', 'workbench.action.closeActiveEditor')
+vscode_map(
+    { 'n', 'x' },
+    '<leader>\\',
+    {
+        'workbench.action.closePanel',
+        'workbench.action.closeSidebar',
+    }
+)
 
 -- Find stuff
 
